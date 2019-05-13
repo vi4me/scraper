fff22cc0cc62c659db3e332398e9af98a7d4c662
vi4me <https://github.com/vi4me/Read.git>
2019-05-13 06:35:53 +0300

    my mechanize

--- scraper.rb
+++ scraper.rb
@@ -3,37 +3,32 @@ require 'date'
 require 'json'
 
 agent = Mechanize.new
-page  = agent.get('http://pitchfork.com/reviews/albums/')
-
-review_links = page.links_with(href: %r{^/reviews/albums/\w+})
-
-review_links = review_links.reject do |link|
-  parent_classes = link.node.parent['class'].split
-  parent_classes.any? { |p| %w[next-container page-number].include?(p) }
-end
+page  = agent.get('https://loveandcarry.com/ua/shop/view/')
+url_img = ('https://loveandcarry.com/')
+review_links = page.links_with(:class => 'bprod')
 
 review_links = review_links[0...4]
 
 reviews = review_links.map do |link|
   review = link.click
-  review_meta = review.search('div#site-content')
-  artist = review_meta.search('h1').text
-  album = review_meta.search('h2').text
-  img = review_meta.search('.single-album-tombstone__art img').attr('src')
-  reviewer = review_meta.search('.review-detail__abstract').text.strip
-  #contents = review_meta.search('.contents').text.strip
-  review_date = review_meta.css('.pub-date').text
-  genre = review_meta.search('.genre-list__link').text
-  score = review_meta.search('.score').text.to_f
+  review_meta = review.search('div.out')
+  title = review_meta.search('h1.title-p').text
+  color = review_meta.search('.title-p-sm').text
+  img = review_meta.search('.psliders__inner img').map{ |i| url_img + i.attr('src').to_s }
+  price = review_meta.search('#item-price').text
+  description = review_meta.css('ul.list li').map{ |i| i.text.strip}
+  feature = review_meta.css('.flist-block__text').map{ |i| i.text.strip}
+  instruction = review_meta.search('.image-instruction__list-block').map{ |i| i.text.gsub(/\s+/, ' ').strip}
+  complectacion = review_meta.search('.descrblock-spec li').map{ |i| i.text.strip}
   {
-    artist: artist,
-    album: album,
-    img: img,
-    reviewer: reviewer,
-    #contents: contents,
-    review_date: review_date,
-    genre: genre,
-    score: score
+    Название: title,
+    Цвет: color,
+    Фото: img,
+    Цена: price,
+    Описание: description,
+    Особенности: feature,
+    Инструкции: instruction,
+    Комплектация: complectacion
   }
 end
 
